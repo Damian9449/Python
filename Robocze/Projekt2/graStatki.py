945e3f7e241260e9019eed542d1bbfda6a5a7710
Damian Polchlopek <damian.polchlopek@gmail.com>
2017-10-26 22:04:08 +0200

    Add final version of project

--- Robocze/Projekt2/graStatki.py
+++ Robocze/Projekt2/graStatki.py
@@ -2,13 +2,13 @@
 
 import os
 from random import randrange
-import time
+import sys
 
 # czyszczenie ekranu
 os.system("clear")
 
 class Map:
-	# zmienne do interfejsu
+	# zmienne interfejsu
 	PURPLE = '\033[95m'
 	BLUE = '\033[94m'
 	GREEN = '\033[92m'
@@ -18,145 +18,49 @@ class Map:
 	UNDERLINE = '\033[4m'
 	NORMAL = '\033[0m'
 	
-	#zmienne potrzebne do wyrysowania map gry
-	myMap = ""
-
+	# zmienna przechowujaca mape
+	myMap = list()
+	
+	# lista list przechowujaca pola okretow
+	allShips = []
+	
 	def __getitem__(self, key):
 		return self.myMap[key]
-
+	
 	def __init__(self): # ok
-		self.myMap += "  ABCDEFGHIJ\n"
-		for tmp_y in range(10):
-			self.myMap += str(tmp_y+1).zfill(2)
-			for tmp_x in range(10):
-				self.myMap += "."
-			self.myMap += "\n"
-	
+		# inicjacja mapy
+		myMapColumn = [self.BLUE+'.'+self.NORMAL for i in range(10)] + [' ']
+		self.myMap = [myMapColumn for i in range(10)]
+		self.myMap.insert(0, [ self.GREEN+chr(x)+self.NORMAL for x in range(65, 75)])
+		self.myMap[0].insert(11, ' ')
+		self.myMap[0].insert(0, '  ')
+		self.myMap.append([' ' for i in range(12)])
+		
+		for i in range(10):
+			self.myMap[i+1] = [self.GREEN+str(i+1).zfill(2)+self.NORMAL] + self.myMap[i+1]
+		
+		# inicjacja listy przechowujacej wylosowane statki
+		self.allShips = [list() for i in range(10)]		
+		
 	def __str__(self): # ok
-		return self.myMap
+		return "Wskazane jest wypisanie mapy za pomoca specjalnej funkcji"
 	
-	def fill_map(self): # ok
+	def print_map(self):
+		for row in self.myMap:
+			for node in row:
+				sys.stdout.write(node)
+			print ""
 	
-		# funkcja sprawdzajaca sasiadow
-		def chceck_neighbors(wsp_x, wsp_y):
-			position = 13*wsp_y + wsp_x + 1
-			
-			# sprawdzanie podstawowych kierunkow
-			up = position - 13
-			down = position + 13
-			left = position - 1
-			right = position + 1
-			
-			# sprawdzanie skosnych kierunkow
-			up_left = position - 14
-			up_right = position - 12
-			down_left = position + 14
-			down_right = position + 12
-			
-			# ochrona przed wyjsciem z zakresu stringa
-			if down > 141:
-				down = 1
-			if down_left > 141:
-				down_left = 1
-			if down_right > 141:
-				down_right = 1
-			
-			if self.myMap[up] != 'X' and self.myMap[down] != 'X' and \
-			   self.myMap[left] != 'X' and self.myMap[right] != 'X' and \
-			   self.myMap[up_left] != 'X' and self.myMap[up_right] != 'X' and \
-			   self.myMap[down_left] != 'X' and self.myMap[down_right] != 'X' and \
-			   self.myMap[position] != 'X':
-			   return True
-			else:
-				return False
-		
-		# umieszczanie na mapie 'jedynek'
-		tmp = 4
-		while tmp > 0:
-			wsp_x = randrange(1,11)
-			wsp_y = randrange(1,11)
-			if chceck_neighbors(wsp_x,wsp_y):
-				self.update_map(wsp_x,wsp_y,'X')
-				tmp -= 1
-				
-		# umieszczanie na mapie 'dwojek'
-		tmp = 3
-		while tmp > 0:
-			direction = randrange(1,3)
-			if direction == 1:
-				wsp_x = randrange(1,11)
-				wsp_y = randrange(2,11)
-				if chceck_neighbors(wsp_x,wsp_y) and chceck_neighbors(wsp_x,wsp_y-1):
-					self.update_map(wsp_x,wsp_y,'X')
-					self.update_map(wsp_x,wsp_y-1,'X')
-					tmp -= 1
-			else:
-				wsp_x = randrange(2,11)
-				wsp_y = randrange(1,11)
-				if chceck_neighbors(wsp_x,wsp_y) and chceck_neighbors(wsp_x-1,wsp_y):
-					self.update_map(wsp_x,wsp_y,'X')
-					self.update_map(wsp_x-1,wsp_y,'X')
-					tmp -= 1
-		
-		# umieszczanie na mapie 'trojek'
-		tmp = 2
-		while tmp > 0:
-			direction = randrange(1,3)
-			if direction == 1:
-				wsp_x = randrange(1,11)
-				wsp_y = randrange(2,10)
-				if chceck_neighbors(wsp_x,wsp_y) and chceck_neighbors(wsp_x,wsp_y-1) and \
-				   chceck_neighbors(wsp_x, wsp_y+1):
-				   
-					self.update_map(wsp_x,wsp_y+1,'X')
-					self.update_map(wsp_x,wsp_y,'X')
-					self.update_map(wsp_x,wsp_y-1,'X')
-					tmp -= 1
-			else:
-				wsp_x = randrange(2,10)
-				wsp_y = randrange(1,11)
-				if chceck_neighbors(wsp_x, wsp_y) and chceck_neighbors(wsp_x-1, wsp_y)and \
-				   chceck_neighbors(wsp_x+1, wsp_y):
-				   
-					self.update_map(wsp_x+1,wsp_y,'X')
-					self.update_map(wsp_x,wsp_y,'X')
-					self.update_map(wsp_x-1,wsp_y,'X')
-					tmp -= 1
-					
-		# umieszczanie na mapie 'czworek'
-		tmp = 1
-		while tmp > 0:
-			direction = randrange(1,3)
-			if direction == 1:
-				wsp_x = randrange(1,11)
-				wsp_y = randrange(2,9)
-				if chceck_neighbors(wsp_x,wsp_y) and chceck_neighbors(wsp_x,wsp_y-1) and \
-				   chceck_neighbors(wsp_x, wsp_y+1) and chceck_neighbors(wsp_x, wsp_y+2):
-					
-					self.update_map(wsp_x,wsp_y+2,'X')
-					self.update_map(wsp_x,wsp_y+1,'X')
-					self.update_map(wsp_x,wsp_y,'X')
-					self.update_map(wsp_x,wsp_y-1,'X')
-					tmp -= 1
-			else:
-				wsp_x = randrange(2,9)
-				wsp_y = randrange(1,11)
-				if chceck_neighbors(wsp_x, wsp_y) and chceck_neighbors(wsp_x-1, wsp_y)and \
-				   chceck_neighbors(wsp_x+1, wsp_y) and chceck_neighbors(wsp_x+2, wsp_y):
-					
-					self.update_map(wsp_x+2,wsp_y,'X')
-					self.update_map(wsp_x+1,wsp_y,'X')
-					self.update_map(wsp_x,wsp_y,'X')
-					self.update_map(wsp_x-1,wsp_y,'X')
-					tmp -= 1
+	# funkcja zamieniajaca znak na mapie
+	def update_map(self, wsp_x, wsp_y, character): #ok
+		self.myMap[wsp_y][wsp_x] = character
 	
 	def manual_fill_map(self):
-		
 		# rozmieszczenie jedynek
 		tmp = 1
 		while tmp != 5:
 			os.system("clear")
-			print self.myMap
+			self.print_map()
 			print "Rozmieszczamy 'jedynke' "
 			wsp_x = raw_input("Podaj wsp x: ")
 			wsp_y = raw_input("Podaj wsp y: ")
@@ -174,6 +78,7 @@ class Map:
 				print "Blad pry konwersji wsp y"
 			
 			self.update_map(i_wsp_x, i_wsp_y, 'X')
+			self.allShips[tmp-1].append([wsp_x, wsp_y])
 			tmp += 1
 		
 		
@@ -181,7 +86,7 @@ class Map:
 		tmp = 1
 		while tmp != 4:
 			os.system("clear")
-			print self.myMap
+			self.print_map()
 			print "Rozmieszczamy 'dwojke' "
 			
 			for i in range(2):
@@ -201,6 +106,7 @@ class Map:
 					print "Blad pry konwersji wsp y"
 				
 				self.update_map(i_wsp_x, i_wsp_y, 'X')
+				self.allShips[tmp+3].append([wsp_x, wsp_y])
 				
 			tmp += 1
 		
@@ -208,7 +114,7 @@ class Map:
 		tmp = 1
 		while tmp != 3:
 			os.system("clear")
-			print self.myMap
+			self.print_map()
 			print "Rozmieszczamy 'trojke' "
 			
 			for i in range(3):
@@ -228,12 +134,13 @@ class Map:
 					print "Blad pry konwersji wsp y"
 				
 				self.update_map(i_wsp_x, i_wsp_y, 'X')
+				self.allShips[tmp+6].append([wsp_x, wsp_y])
 			
 			tmp += 1
-				
+			
 			
 		os.system("clear")
-		print self.myMap
+		self.print_map()
 		# rozmieszczenie czworki
 		print "Rozmieszczamy 'czworke' "
 		for i in range(4):
@@ -253,16 +160,148 @@ class Map:
 				print "Blad pry konwersji wsp y"
 			
 			self.update_map(i_wsp_x, i_wsp_y, 'X')
-	
-	# funkcja zamieniajaca znak na mapie
-	def update_map(self, wsp_x, wsp_y, character): #ok
-		position = 13*wsp_y + wsp_x + 1
-		tmp_map = self.myMap
-		self.myMap = tmp_map[:position]
-		self.myMap += character
-		self.myMap += tmp_map[position+1:]
-
+			self.allShips[9].append([wsp_x, wsp_y])
 		
+		self.print_map()
+	
+	def fill_map(self): # ok
+	
+		# funkcja sprawdzajaca sasiadow
+		def chceck_neighbors(wsp_x, wsp_y):
+			# sprawdzanie podstawowych kierunkow
+			x_up = wsp_x
+			y_up = wsp_y - 1
+			x_down = wsp_x
+			y_down = wsp_y + 1
+			x_left = wsp_x - 1
+			y_left = wsp_y
+			x_right = wsp_x + 1
+			y_right = wsp_y
+			
+			# sprawdzanie skosnych kierunkow
+			x_up_left = wsp_x - 1
+			y_up_left = wsp_y - 1
+			x_up_right = wsp_x + 1
+			y_up_right = wsp_y - 1
+			x_down_left = wsp_x - 1
+			y_down_left = wsp_y + 1
+			x_down_right = wsp_x + 1
+			y_down_right = wsp_y + 1
+			
+			if self.myMap[y_up][x_up] != 'X' and self.myMap[y_down][x_down] != 'X' and \
+			   self.myMap[y_left][x_left] != 'X' and self.myMap[y_right][x_right] != 'X' and \
+			   self.myMap[y_up_left][x_up_left] != 'X' and self.myMap[y_up_right][x_up_right] != 'X' and \
+			   self.myMap[y_down_left][x_down_left] != 'X' and self.myMap[y_down_right][x_down_right] != 'X' and \
+			   self.myMap[wsp_y][wsp_x] != 'X':
+			   return True
+			else:
+				return False
+		
+		# umieszczanie na mapie 'jedynek'
+		tmp = 4
+		while tmp > 0:
+			wsp_x = randrange(1,11)
+			wsp_y = randrange(1,11)
+			if chceck_neighbors(wsp_x,wsp_y):
+				self.update_map(wsp_x,wsp_y,'X')
+				self.allShips[tmp-1].append([wsp_x, wsp_y])
+				tmp -= 1
+				
+		# umieszczanie na mapie 'dwojek'
+		tmp = 3
+		while tmp > 0:
+			direction = randrange(1,3)
+			if direction == 1:
+				wsp_x = randrange(1,11)
+				wsp_y = randrange(2,11)
+				if chceck_neighbors(wsp_x,wsp_y) and chceck_neighbors(wsp_x,wsp_y-1):
+					self.update_map(wsp_x,wsp_y,'X')
+					self.update_map(wsp_x,wsp_y-1,'X')
+					self.allShips[tmp+3].append([wsp_x, wsp_y])
+					self.allShips[tmp+3].append([wsp_x, wsp_y-1])
+					tmp -= 1
+			else:
+				wsp_x = randrange(2,11)
+				wsp_y = randrange(1,11)
+				if chceck_neighbors(wsp_x,wsp_y) and chceck_neighbors(wsp_x-1,wsp_y):
+					self.update_map(wsp_x,wsp_y,'X')
+					self.update_map(wsp_x-1,wsp_y,'X')
+					self.allShips[tmp+3].append([wsp_x, wsp_y])
+					self.allShips[tmp+3].append([wsp_x, wsp_y-1])
+					tmp -= 1
+		
+		# umieszczanie na mapie 'trojek'
+		tmp = 2
+		while tmp > 0:
+			direction = randrange(1,3)
+			if direction == 1:
+				wsp_x = randrange(1,11)
+				wsp_y = randrange(2,10)
+				if chceck_neighbors(wsp_x,wsp_y) and chceck_neighbors(wsp_x,wsp_y-1) and \
+				   chceck_neighbors(wsp_x, wsp_y+1):
+				   
+					self.update_map(wsp_x,wsp_y+1,'X')
+					self.update_map(wsp_x,wsp_y,'X')
+					self.update_map(wsp_x,wsp_y-1,'X')
+					
+					self.allShips[tmp+6].append([wsp_x, wsp_y+1])
+					self.allShips[tmp+6].append([wsp_x, wsp_y])
+					self.allShips[tmp+6].append([wsp_x, wsp_y-1])
+					tmp -= 1
+			else:
+				wsp_x = randrange(2,10)
+				wsp_y = randrange(1,11)
+				if chceck_neighbors(wsp_x, wsp_y) and chceck_neighbors(wsp_x-1, wsp_y)and \
+				   chceck_neighbors(wsp_x+1, wsp_y):
+				   
+					self.update_map(wsp_x+1,wsp_y,'X')
+					self.update_map(wsp_x,wsp_y,'X')
+					self.update_map(wsp_x-1,wsp_y,'X')
+					
+					self.allShips[tmp+6].append([wsp_x+1, wsp_y])
+					self.allShips[tmp+6].append([wsp_x, wsp_y])
+					self.allShips[tmp+6].append([wsp_x-1, wsp_y])
+					tmp -= 1
+					
+		# umieszczanie na mapie 'czworek'
+		tmp = 1
+		while tmp > 0:
+			direction = randrange(1,3)
+			if direction == 1:
+				wsp_x = randrange(1,11)
+				wsp_y = randrange(2,9)
+				if chceck_neighbors(wsp_x,wsp_y) and chceck_neighbors(wsp_x,wsp_y-1) and \
+				   chceck_neighbors(wsp_x, wsp_y+1) and chceck_neighbors(wsp_x, wsp_y+2):
+					
+					self.update_map(wsp_x,wsp_y+2,'X')
+					self.update_map(wsp_x,wsp_y+1,'X')
+					self.update_map(wsp_x,wsp_y,'X')
+					self.update_map(wsp_x,wsp_y-1,'X')
+					
+					self.allShips[9].append([wsp_x, wsp_y+2])
+					self.allShips[9].append([wsp_x, wsp_y+1])
+					self.allShips[9].append([wsp_x, wsp_y])
+					self.allShips[9].append([wsp_x, wsp_y-1])
+					
+					tmp -= 1
+			else:
+				wsp_x = randrange(2,9)
+				wsp_y = randrange(1,11)
+				if chceck_neighbors(wsp_x, wsp_y) and chceck_neighbors(wsp_x-1, wsp_y)and \
+				   chceck_neighbors(wsp_x+1, wsp_y) and chceck_neighbors(wsp_x+2, wsp_y):
+					
+					self.update_map(wsp_x+2,wsp_y,'X')
+					self.update_map(wsp_x+1,wsp_y,'X')
+					self.update_map(wsp_x,wsp_y,'X')
+					self.update_map(wsp_x-1,wsp_y,'X')
+					
+					self.allShips[9].append([wsp_x+2, wsp_y])
+					self.allShips[9].append([wsp_x+1, wsp_y])
+					self.allShips[9].append([wsp_x, wsp_y])
+					self.allShips[9].append([wsp_x-1, wsp_y])
+					
+					tmp -= 1
+	
 class Game:
 	# zmienne do interfejsu
 	PURPLE = '\033[95m'
@@ -274,8 +313,32 @@ class Game:
 	UNDERLINE = '\033[4m'
 	NORMAL = '\033[0m'
 	
-	autoFillMap = False
+	autoFillMap = True
 	
+	my_map = Map()
+	computer_map = Map()
+	tmp_map = Map()
+	
+	# POPRAWIC WYRYSOWYWANIE STATKOW
+	def color_ships(self, tmp_map, computer_map):
+		for ship in self.computer_map.allShips:
+			triger = True;
+			for area in range(len(ship)):
+				x_tmp = ship[area][0]
+				y_tmp = ship[area][1]
+				
+				if self.tmp_map[y_tmp][x_tmp] == 'X' and self.computer_map[y_tmp][x_tmp] == 'X':
+					print "True"
+				else:
+					triger = False
+					break
+				
+			if triger:
+				for area in range(len(ship)):
+					x_tmp = ship[area][0]
+					y_tmp = ship[area][1]
+					self.tmp_map[y_tmp][x_tmp] = '@'
+				
 	def start_game(self):
 		menu = 1
 		key = 'f'
@@ -343,17 +406,22 @@ class Game:
 				redraw = False	
 		
 	def print_all_map(self):
+	
 		os.system("clear")
 		pixel = 70
-		print "========================".center(pixel)
-		print self.YELLOW + "Mapa przeciwnika".center(pixel) + self.NORMAL	
-		print "========================".center(pixel)
-		print self.computer_map
+		# print self.RED+"========================".center(pixel),self. NORMAL
+		# print self.YELLOW + "Computer".center(pixel) + self.NORMAL	
+		# print self.RED+"========================".center(pixel), self.NORMAL
+		# self.computer_map.print_map()
+		print self.RED+"========================".center(pixel),self. NORMAL
+		print self.YELLOW + "Twoje strzaly".center(pixel) + self.NORMAL	
+		print self.RED+"========================".center(pixel), self.NORMAL
+		self.tmp_map.print_map()
 		print "\n"
-		print "========================".center(pixel)
+		print self.RED+"========================".center(pixel), self.NORMAL
 		print self.YELLOW + "Twoja mapa".center(pixel) + self.NORMAL	
-		print "========================".center(pixel)
-		print self.my_map
+		print self.RED+"========================".center(pixel), self.NORMAL
+		self.my_map.print_map()
 		
 	def run_game(self):
 		wsp_x = 0
@@ -371,7 +439,7 @@ class Game:
 		
 		self.my_map = Map()
 		self.computer_map = Map()
-		self.tmp = Map()
+		self.tmp_map = Map()
 			
 		def check_win(): #ok
 			if mySunkenShips != 20 and computerSunkenShips != 20:
@@ -392,7 +460,6 @@ class Game:
 			self.print_all_map()
 			
 			if nextShoot == 1:
-			
 				wsp_x = raw_input("Podaj wsp x: ")
 				wsp_y = raw_input("Podaj wsp y: ")
 				
@@ -408,35 +475,35 @@ class Game:
 				except ValueError:                  # kod obslugujacy bledy
 					print "Blad pry konwersji wsp y"
 				
+				
 				# ustalenie kto wykonuje kolejny ruchu
-				position = 13 * i_wsp_y + i_wsp_x + 1
-				if self.computer_map[position] == 'X':
+				# sprwdzenie czy zostal trafiony statek
+				if self.computer_map[i_wsp_y][i_wsp_x] == 'X':
+					self.tmp_map.update_map(i_wsp_x, i_wsp_y, 'X')
 					computerSunkenShips += 1
 					nextShoot = 1
 				else:
 					nextShoot = 2
-				
-				# wykonanie mojego ruchu
-				self.computer_map.update_map(i_wsp_x, i_wsp_y, '*')
-				
-			
+					# jesli statek nie nostaltrafiony zaznaczamy tylko strzal
+					self.tmp_map.update_map(i_wsp_x, i_wsp_y, '*')
+					
 			if nextShoot == 2:
 				#wykonanie ruchu komputera
 				while True:
 					i_wsp_x = randrange(1,11)
 					i_wsp_y = randrange(1,11)
-					position = 13 * i_wsp_y + i_wsp_x + 1
-					if self.my_map[position] != '*':
+					if self.my_map[i_wsp_y][i_wsp_x] != '*':
 						#sprawdzenie czy nastapilo trafienie w statek
-						if self.my_map[position] == 'X':
+						if self.my_map[i_wsp_y][i_wsp_x] == 'X':
 							mySunkenShips += 1
 						self.my_map.update_map(i_wsp_x, i_wsp_y, '*')
 						break
 				
 				#kolejka gracza
 				nextShoot = 1
+				
+			self.color_ships(self.tmp_map, self.computer_map)
 			
-			time.sleep(0.05)
 		
 		# sprawdzenie kto wygral
 		if mySunkenShips >= 20:
@@ -448,7 +515,6 @@ class Game:
 			print "q - menu glowne  ".center(pixel)
 			print "=====================".center(pixel)
 			print self.NORMAL
-			
 			
 		if computerSunkenShips >= 20:
 			os.system("clear")
